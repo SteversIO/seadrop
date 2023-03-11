7065fd3973c5b5005b1e4c31d7f6d70b56631049
Stevers <me@stevers.io>
2023-03-11 18:12:05 -0500

    Adds script for mnemonic to wallets

---
+++ scripts/wallets.from.mnemonic.ts
@@ -0,0 +1,21 @@
+import { Wallet } from "ethers";
+
+async function main() {
+  console.log("Converting mnemonic to wallet keys");
+  let mnemonic = "candy maple cake sugar pudding cream honey rich smooth crumble sweet treat";
+  
+  const addresses = [];
+  for(let i=0; i<10; i++) {
+    const bip32derivation = `m/44'/60'/0'/0/${i}`
+    const wallet = Wallet.fromMnemonic(mnemonic, bip32derivation);
+    addresses.push(wallet);
+  }
+  console.log("Addresses from mnemonic are: ", addresses);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
